13e1ab872468a24597539ebe889d5f5c9ab9e416
dmglynn <dmglynn@ncsu.edu>
2023-10-30 18:14:41 -0400

    tooltip

--- src/ui/equip_mod_tile.gd
+++ src/ui/equip_mod_tile.gd
@@ -3,8 +3,12 @@ extends TextureButton
 
 
 var mod : Modification
+var menu: EquipMenu
+
 
 func populate_mod(modification: Modification):
 	mod = modification
 	
 	$Label.text = mod.name
+	
+	tooltip_text = mod.description
