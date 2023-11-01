584a5a8ef962edf9f623240e0ebaf6f722841ba8
TheModernSMoore <themodernsmoore@gmail.com>
2023-11-01 16:09:16 -0400

    Updated spawn_point to not spawn until it needs to

--- src/spawn_point.gd
+++ src/spawn_point.gd
@@ -3,6 +3,7 @@ extends Node2D
 const ENEMY = preload("res://src/actors/enemy.tscn")
 
 const OFFSCREEN_PLUSALITTLE = 175
+const OFFSCREEN_PLUSSOMEMORE = 275
 
 var curr_enemy = null
 
@@ -15,7 +16,11 @@ func _ready():
 func _process(delta):
 	if !is_instance_valid(get_tree().get_first_node_in_group("player")):
 		return
-	if curr_enemy == null and (get_tree().get_first_node_in_group("player").position - position).length() > OFFSCREEN_PLUSALITTLE:
+	var player := get_tree().get_first_node_in_group("player") as Player
+	var player_pos = player.position
+	if curr_enemy == null and (player_pos - position).length() > OFFSCREEN_PLUSALITTLE and (player_pos - position).length() < OFFSCREEN_PLUSSOMEMORE:
 		curr_enemy = ENEMY.instantiate()
 		curr_enemy.position = position
 		get_parent().add_child(curr_enemy)
+	if curr_enemy != null and (player_pos - curr_enemy.position).length() >= OFFSCREEN_PLUSSOMEMORE:
+		curr_enemy.queue_free()
