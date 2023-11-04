4eca998ca96f1a1ea31812afde52fa156403a22d
dmglynn <dmglynn@ncsu.edu>
2023-11-04 14:40:02 -0400

    slight balancing update

--- src/spawn_point.gd
+++ src/spawn_point.gd
@@ -7,6 +7,8 @@ const OFFSCREEN_PLUSSOMEMORE = 300
 
 var curr_enemy = null
 
+@onready var respawn_timer = $RespawnTimer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,9 +20,10 @@ func _process(delta):
 		return
 	var player := get_tree().get_first_node_in_group("player") as Player
 	var player_pos = player.global_position
-	if curr_enemy == null and (player_pos - global_position).length() > OFFSCREEN_PLUSALITTLE and (player_pos - global_position).length() < OFFSCREEN_PLUSSOMEMORE:
+	if respawn_timer.is_stopped() && curr_enemy == null and (player_pos - global_position).length() > OFFSCREEN_PLUSALITTLE and (player_pos - global_position).length() < OFFSCREEN_PLUSSOMEMORE:
 		curr_enemy = ENEMY.instantiate()
 		curr_enemy.global_position = global_position
 		get_parent().add_child(curr_enemy)
+		respawn_timer.start()
 	if curr_enemy != null and (player_pos - curr_enemy.global_position).length() >= OFFSCREEN_PLUSSOMEMORE:
 		curr_enemy.queue_free()
